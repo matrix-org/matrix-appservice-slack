8590c7a32686ce162b3f995c017638985316ad3e
Daniel Wagner-Hall <daniel@matrix.org>
2015-09-23 16:11:23 +0100

    Support sending images slack -> matrix
    Only supported if a slack_master_token is specified, which is a token
    with permission to view history of each room being bridged.

--- tests/slack-hook-handler.spec.js
+++ tests/slack-hook-handler.spec.js
@@ -11,14 +11,24 @@ function makeRequest(user, text) {
 }
 
 describe("SlackHookHandler.handle", function() {
+    var requestLib;
     var handler;
     var intent;
+    var config;
+    var timestamp = "1443017615.000193";
+
+    function assertNoMessagesSent() {
+        expect(intent.sendMessage).not.toHaveBeenCalled();
+        expect(intent.sendText).not.toHaveBeenCalled();
+    }
 
     beforeEach(function() {
-        var config = {
+        requestLib = {};
+        config = {
             homeserver: {
                 server_name: "foo.bar"
-            }
+            },
+            slack_master_token: "leavinganote"
         };
         var rooms = {
             knowsSlackChannel: function(channel_name) {
@@ -28,19 +38,19 @@ describe("SlackHookHandler.handle", function() {
                 return channel_name == "slackchan" ? "!room:host" : null;
             }
         };
-        intent = {};
+        intent = createSpyObj("intent", ["sendMessage", "sendText"]);
         var bridge = {
             getIntent: function() {
                 return intent;
             }
         };
-        handler = new SlackHookHandler(config, rooms, bridge);
+        handler = new SlackHookHandler(requestLib, config, rooms, bridge);
     });
 
     describe("handle text messages", function() {
         it("ignores slackbot", function() {
             handler.handle(makeRequest("USLACKBOT"));
-            // If any methods had been called on intent, we would have thrown.
+            assertNoMessagesSent();
         });
         it("sends text messages", function() {
             intent = createSpyObj("intent", ["sendText"]);
@@ -50,10 +60,169 @@ describe("SlackHookHandler.handle", function() {
 
         });
         it("unescapes special characters", function() {
-            intent = createSpyObj("intent", ["sendText"]);
             handler.handle(makeRequest("GOB", "&lt;special &amp; characters&gt;"));
             expect(intent.sendText).toHaveBeenCalledWith(
                 "!room:host", "<special & characters>");
+        });
+    });
+
+    function mockChannelHistory(reply) {
+        requestLib.post = function(url, params, cb) {
+            expect(url).toEqual("https://slack.com/api/channels.history");
+            expect(params).toEqual({form: {
+                channel: "slackchan",
+                latest: timestamp,
+                oldest: timestamp,
+                inclusive: "1",
+                token: "leavinganote",
+            }});
+            cb(null, undefined, JSON.stringify(reply));
+        };
+    }
+
+    function receiveImage() {
+        handler.handle({
+                "channel_id": "slackchan",
+                "channel_name": "tantamount_studios",
+                "timestamp": "1443017615.000193",
+                "user_id": "MF",
+                "user_name": "maeby"
+        });
+    }
+
+    describe("handles image message", function() {
+        it("ignores images if no master token set", function() {
+            delete config.slack_master_token;
+            requestLib = createSpyObj("requestLib", ["post"]);
+            receiveImage();
+            expect(requestLib.post).not.toHaveBeenCalled();
+            assertNoMessagesSent();
+        });
+        it("sends image", function() {
+            mockChannelHistory({
+                ok: true,
+                messages: [{
+                    type: "message",
+                    subtype: "file_share",
+                    file: {
+                        mimetype: "image/jpeg",
+                        size: 123,
+                        url: "http://almost.cousins/kissing.jpg",
+                        title: "GM&M",
+                        original_w: 1024,
+                        original_h: 768
+                    }
+                }]
+            });
+            receiveImage();
+            expect(intent.sendMessage).toHaveBeenCalledWith(
+                "!room:host", {
+                    msgtype: "m.image",
+                    url: "http://almost.cousins/kissing.jpg",
+                    body: "GM&M",
+                    info: {
+                        mimetype: "image/jpeg",
+                        size: 123,
+                        w: 1024,
+                        h: 768
+                    }
+                }
+            );
+        });
+        it("sends image with thumbnail", function() {
+            mockChannelHistory({
+                ok: true,
+                messages: [{
+                    type: "message",
+                    subtype: "file_share",
+                    file: {
+                        mimetype: "image/jpeg",
+                        size: 123,
+                        url: "http://almost.cousins/kissing.jpg",
+                        title: "GM&M",
+                        original_w: 1024,
+                        original_h: 768,
+                        thumb_360: "http://almost.cousins/kissing_360.jpg",
+                        thumb_360_w: 360,
+                        thumb_360_h: 240
+                    }
+                }]
+            });
+            receiveImage();
+            expect(intent.sendMessage).toHaveBeenCalledWith(
+                "!room:host", {
+                    msgtype: "m.image",
+                    url: "http://almost.cousins/kissing.jpg",
+                    thumbnail_url: "http://almost.cousins/kissing_360.jpg",
+                    body: "GM&M",
+                    info: {
+                        mimetype: "image/jpeg",
+                        size: 123,
+                        w: 1024,
+                        h: 768
+                    },
+                    thumbnail_info: {
+                        w: 360,
+                        h: 240
+                    }
+                }
+            );
+        });
+        it("sends image with comment", function() {
+            mockChannelHistory({
+                ok: true,
+                messages: [{
+                    type: "message",
+                    subtype: "file_share",
+                    file: {
+                        mimetype: "image/jpeg",
+                        size: 123,
+                        url: "http://almost.cousins/kissing.jpg",
+                        title: "GM&M",
+                        original_w: 1024,
+                        original_h: 768,
+                        initial_comment: {
+                            comment: "&lt;It's only 63 minutes long&gt;"
+                        }
+                    }
+                }]
+            });
+            receiveImage();
+            expect(intent.sendMessage).toHaveBeenCalledWith(
+                "!room:host", {
+                    msgtype: "m.image",
+                    url: "http://almost.cousins/kissing.jpg",
+                    body: "GM&M",
+                    info: {
+                        mimetype: "image/jpeg",
+                        size: 123,
+                        w: 1024,
+                        h: 768
+                    }
+                }
+            );
+            expect(intent.sendText).toHaveBeenCalledWith(
+                "!room:host",
+                "<It's only 63 minutes long>"
+            );
+        });
+        it("ignores non-images", function() {
+            mockChannelHistory({
+                ok: true,
+                messages: [{
+                    type: "message",
+                    subtype: "file_share",
+                    file: {
+                        mimetype: "application/monkeys",
+                        size: 123,
+                        url: "http://almost.cousins/kissing.jpg",
+                        title: "GM&M",
+                        original_w: 1024,
+                        original_h: 768
+                    }
+                }]
+            });
+            assertNoMessagesSent();
         });
     });
 
@@ -61,6 +230,7 @@ describe("SlackHookHandler.handle", function() {
 
 describe("SlackHookHandler", function() {
     it("gets intent", function() {
+        var requestLib = undefined;
         var config = {
             username_prefix: "prefix_",
             homeserver: {
@@ -69,7 +239,7 @@ describe("SlackHookHandler", function() {
         };
         var rooms = undefined;
         var bridge = createSpyObj("bridge", ["getIntent"]);
-        var handler = new SlackHookHandler(config, rooms, bridge)
+        var handler = new SlackHookHandler(requestLib, config, rooms, bridge)
         handler.getIntent("foo");
         expect(bridge.getIntent).toHaveBeenCalledWith("@prefix_foo:my.homeserver");
     });
