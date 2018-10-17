e19921932bb44d1d63b1b675be7ef1e4e05f7e82
Stuart Mumford <stuart@cadair.com>
2018-10-17 15:37:30 -0600

    var makes for a sad halfy

--- tests/test-subsitutions.spec.js
+++ tests/test-subsitutions.spec.js
@@ -1,27 +1,27 @@
-var subsitutions = require("../lib/substitutions");
+const subsitutions = require("../lib/substitutions");
 
 describe ("Plain Text Mentions", () => {
-    var displaymap = {Stuart: "U1111", Cadair: "U2222", "Stuart Mumford": "U3333"}
+    const displaymap = {Stuart: "U1111", Cadair: "U2222", "Stuart Mumford": "U3333"}
     it("replace unique nick", () => {
-        var new_string = subsitutions.replacementFromDisplayMap("@Cadair", displaymap);
+        const new_string = subsitutions.replacementFromDisplayMap("@Cadair", displaymap);
         expect(new_string).toBe("<@U2222>");
     });
     it("replace shortest non-unique", () => {
-        var new_string = subsitutions.replacementFromDisplayMap("@Stuart", displaymap);
+        const new_string = subsitutions.replacementFromDisplayMap("@Stuart", displaymap);
         expect(new_string).toBe("<@U1111>");
     });
     it("replace longest non-unique", () => {
-        var new_string = subsitutions.replacementFromDisplayMap("@Stuart Mumford", displaymap);
+        const new_string = subsitutions.replacementFromDisplayMap("@Stuart Mumford", displaymap);
         expect(new_string).toBe("<@U3333>");
     });
 })
 
 describe ("Make First Word Map", () => {
-    var displaymap = {Stuart: "U1111", Cadair: "U2222", "Stuart Mumford": "U3333"}
-    var expectedfirstwords = {Stuart: [{Stuart: "U1111"}, {"Stuart Mumford": "U3333"}],
-                              Cadair: [{Cadair: "U2222"}]};
+    const displaymap = {Stuart: "U1111", Cadair: "U2222", "Stuart Mumford": "U3333"}
+    const expectedfirstwords = {Stuart: [{Stuart: "U1111"}, {"Stuart Mumford": "U3333"}],
+                                Cadair: [{Cadair: "U2222"}]};
     it("Make the First Word Map", () => {
-        var firstwords = subsitutions.makeFirstWordMap(displaymap);
+        const firstwords = subsitutions.makeFirstWordMap(displaymap);
         expect(firstwords).toEqual(expectedfirstwords);
     });
 })
