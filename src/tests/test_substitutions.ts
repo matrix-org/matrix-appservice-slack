ebc9d5dd12b0f760009605c99967785d97e6b0a9
Half-Shot <will@half-shot.uk>
2019-07-24 14:36:02 +0100

    use alice,bob..

--- src/tests/test_substitutions.ts
+++ src/tests/test_substitutions.ts
@@ -4,31 +4,31 @@ import { default as subsitutions, replacementFromDisplayMap } from "../substitut
 import { expect } from "chai";
 
 const displayMap = {
-    "Cadair": "U2222",
-    "Stuart": "U1111",
-    "Stuart Mumford": "U3333",
+    "Alice": "U2222",
+    "Bob": "U1111",
+    "Bob Charlie": "U3333",
 };
 
 describe("Substitutions", () => {
     describe("replacementFromDisplayMap", () => {
         it("should replace a unique nick", () => {
-            const newString = replacementFromDisplayMap("@Cadair", displayMap);
+            const newString = replacementFromDisplayMap("@Alice", displayMap);
             expect(newString).to.equal("<@U2222>");
         });
         it("should replace the shortest non-unique", () => {
-            const newString = replacementFromDisplayMap("@Stuart", displayMap);
+            const newString = replacementFromDisplayMap("@Bob", displayMap);
             expect(newString).to.equal("<@U1111>");
         });
         it("should replace the longest non-unique", () => {
-            const newString = replacementFromDisplayMap("@Stuart Mumford", displayMap);
+            const newString = replacementFromDisplayMap("@Bob Charlie", displayMap);
             expect(newString).to.equal("<@U3333>");
         });
     });
     describe("makeFirstWordMap", () => {
         const expectedFirstWords = {
-            Cadair: [{Cadair: "U2222"}],
-            Stuart: [{Stuart: "U1111"},
-            {"Stuart Mumford": "U3333"}],
+            Alice: [{Alice: "U2222"}],
+            Bob: [{Bob: "U1111"},
+            {"Bob Charlie": "U3333"}],
         };
         it("should make the first word map", () => {
             const firstWords = subsitutions.makeFirstWordMap(displayMap);
