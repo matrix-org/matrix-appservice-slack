834bbd7009029c3bff3eef8660aacbe201223ebb
Half-Shot <will@half-shot.uk>
2019-08-12 15:09:39 +0100

    Add integration tests

---
+++ src/tests/integration/SlackToMatrixTest.ts
@@ -0,0 +1,68 @@
+/*
+Copyright 2019 The Matrix.org Foundation C.I.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import { SlackHookHandler } from "../../SlackHookHandler";
+import { Main } from "../../Main";
+import { FakeMain } from "../utils/fakeMain";
+import { ServerResponse } from "http";
+import { expect } from "chai";
+import { SlackEventHandler } from "../../SlackEventHandler";
+
+// tslint:disable: no-unused-expression no-any
+
+class FakeResponder {
+    public head?: {code: number, status: string};
+    public hasEnded: boolean = false;
+
+    public writeHead(code: number, status: string) {
+        this.head = {code, status};
+    }
+
+    public end() {
+        this.hasEnded = true;
+    }
+}
+function constructHarness() {
+    const main = new FakeMain() ;
+    const hooks = new SlackHookHandler(main as unknown as Main);
+    return { eventHandler: hooks.eventHandler, main };
+}
+
+let harness: { eventHandler: SlackEventHandler, main: FakeMain };
+
+describe("SlackToMatrix", () => {
+
+    beforeEach(() => {
+        harness = constructHarness();
+    });
+
+    it("will drop slack events that have an unknown type", async () => {
+        const responder = new FakeResponder();
+        await harness.eventHandler.handle({
+            team_id: "12345",
+            name: "",
+            type: "unknown type",
+            event: {
+                type: "faketype",
+                channel: "fakechannel",
+                ts: "12345",
+            },
+        }, responder as unknown as ServerResponse);
+        expect(responder.hasEnded).to.be.true;
+        expect(responder.head).to.deep.equal({code: 200, status: "OK"});
+        expect(harness.main.timerFinished.remote_request_seconds).to.be.equal("dropped");
+    });
+});
