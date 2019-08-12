374f66798f1464f87d90efe530d655a7c28a5cb6
Half-Shot <will@half-shot.uk>
2019-08-12 13:36:11 +0100

    Minor things

--- src/AdminCommands.ts
+++ src/AdminCommands.ts
@@ -41,9 +41,10 @@ export class AdminCommands {
             this.help,
     ];
     constructor(private main: Main) {
-        this.yargs = yargs.parserConfiguration({
+        this.yargs = yargs.parserConfiguration({})
+        .version(false)
+        .help(false); // We provide our own help, and version is not required.
 
-        }).version(false).help(false); // We provide our own help, and version is not required.
         this.commands.forEach((cmd) => {
             this.yargs = this.yargs.command(cmd.command, cmd.description, ((yg) => {
                 if (cmd.options) {
