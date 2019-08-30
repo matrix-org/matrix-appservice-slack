02cf83dc0520417df9ba7247175b464d8147ec3d
Half-Shot <will@half-shot.uk>
2019-08-30 15:40:12 +0100

    Add datastore/Models.ts

---
+++ src/datastore/Models.ts
@@ -0,0 +1,68 @@
+import { BridgedRoom } from "../BridgedRoom";
+import { SlackGhost } from "../SlackGhost";
+import { MatrixUser } from "matrix-appservice-bridge";
+
+export interface RoomEntry {
+    id: string;
+    matrix_id: string;
+    remote_id: string;
+    remote: {
+        slack_team_id?: string;
+        slack_bot_token?: string;
+        access_scopes: string[];
+        access_token: string;
+        slack_bot_id: string;
+        id: string;
+        name: string;
+        slack_team_domain: string;
+        slack_user_id: string;
+        slack_user_token: string;
+        webhook_uri: string;
+    };
+}
+
+export interface UserEntry {
+    id: string;
+    matrix_id: string;
+    display_name: string;
+    get: (key: string) => string;
+    set: (key: string, value: string) => void;
+}
+
+export interface EventEntry {
+    eventId: string;
+    roomId: string;
+    slackChannelId: string;
+    slackTs: string;
+    _extras: EventEntryExtra;
+}
+
+export interface EventEntryExtra {
+    slackThreadMessages?: string[];
+}
+
+export interface TeamEntry {
+    team_id: string;
+    bot_token: string;
+    team_name: string;
+    user_id: string;
+}
+
+export interface Datastore {
+    upsertUser(user: SlackGhost): Promise<void>;
+    getUser(id: string): Promise<UserEntry|null>;
+    getMatrixUser(userId: string): Promise<MatrixUser|null>;
+    storeMatrixUser(user: MatrixUser): Promise<void>;
+
+    upsertRoom(room: BridgedRoom): Promise<void>;
+    deleteRoom(id: string): Promise<void>;
+    getAllRooms(): Promise<RoomEntry[]>;
+
+    upsertEvent(roomId: string, eventId: string, channelId: string, ts: string, extras?: EventEntryExtra): Promise<void>;
+    upsertEvent(roomIdOrEntry: EventEntry): Promise<void>;
+    getEventByMatrixId(roomId: string, eventId: string): Promise<EventEntry|null>;
+    getEventBySlackId(channelId: string, ts: string): Promise<EventEntry|null>;
+
+    upsertTeam(teamId: string, botToken: string, teamName: string, botId: string);
+    getTeam(teamId: string): Promise<TeamEntry>;
+}
