cc96791f69a1450ab84996eb1f9610cadd4c5143
Half-Shot <will@half-shot.uk>
2019-09-12 15:21:49 +0100

    Tweak SlackClientFactory for review

--- src/SlackClientFactory.ts
+++ src/SlackClientFactory.ts
@@ -26,59 +26,49 @@ export class SlackClientFactory {
      * Gets a team entry from the datastore and checks if the token
      * is safe to use.
      * @param teamId The slack teamId to check.
+     * @throws If the team is not safe to use
      */
-    public async isTeamStatusOkay(teamId: string): Promise<boolean> {
+    public async isTeamStatusOkay(teamId: string) {
         const storedTeam = await this.datastore.getTeam(teamId);
         if (!storedTeam) {
-            log.warn(`Team ${teamId} is not ready: No team found in store`);
-            return false;
+            throw Error(`Team ${teamId} is not ready: No team found in store`);
         }
         if (storedTeam.status === "bad_auth") {
-            log.warn(`Team ${teamId} is not ready: Team previously failed to auth and has been disabled`);
-            return false;
+            throw Error(`Team ${teamId} is not ready: Team previously failed to auth and has been disabled`);
         }
         if (storedTeam.status === "archived") {
-            log.warn(`Team ${teamId} is not ready: Team is archived.`);
-            return false;
+            throw Error(`Team ${teamId} is not ready: Team is archived.`);
         }
         if (!storedTeam.bot_token) {
-            log.warn(`Team ${teamId} is not ready: No token stored.`);
-            return false;
+            throw Error(`Team ${teamId} is not ready: No token stored.`);
         }
-        return true;
     }
 
     /**
      * Gets a WebClient for a given teamId. If one has already been
      * created, the cached client is returned.
-     * This may tho
      * @param teamId The slack team_id.
-     * @throws If the team token is unauthorized.
+     * @throws If the team client fails to be created.
      */
     public async getTeamClient(teamId: string): Promise<WebClient> {
         if (this.teamClients.has(teamId)) {
             return this.teamClients.get(teamId)!;
         }
-        const storedTeam = await this.datastore.getTeam(teamId);
-        if (!storedTeam) {
-            throw Error("No team found in store");
+        const teamEntry = await this.datastore.getTeam(teamId);
+        if (!teamEntry) {
+            throw Error(`No team found in store for ${teamId}`);
         }
         // Check that the team is actually usable.
-        if (!await this.isTeamStatusOkay(teamId)) {
-            throw Error("Team status is not okay");
-        }
-        // This exists because the previous statement passed.
-        const teamEntry = (await this.datastore.getTeam(teamId))!;
+        await this.isTeamStatusOkay(teamId);
+
         log.info("Creating new team client for", teamId);
         try {
-            const { slackClient, team } = await this.createTeamClient(teamEntry.bot_token!);
+            const { slackClient, team, user } = await this.createTeamClient(teamEntry.bot_token!);
             // Call this to get our identity.
-            const authRes = (await slackClient.auth.test()) as AuthTestResponse;
-            const userRes = (await slackClient.users.info({user: authRes.user_id})) as UsersInfoResponse;
             teamEntry.domain = team.domain;
             teamEntry.name = team.name;
-            teamEntry.bot_id = userRes.user!.profile!.bot_id!;
-            teamEntry.user_id = userRes.user!.id!;
+            teamEntry.bot_id = user.user!.profile!.bot_id!;
+            teamEntry.user_id = user.user!.id!;
             teamEntry.status = "ok";
             this.teamClients.set(teamId, slackClient);
             return slackClient;
@@ -104,42 +94,29 @@ export class SlackClientFactory {
         let teamRes: {id: string, name: string, domain: string};
         let botId: string;
         let userId: string;
-        let existingTeam: TeamEntry|undefined;
         try {
-            const { slackClient, team } = await this.createTeamClient(token);
+            const { team , auth, user} = await this.createTeamClient(token);
             // Call this to get our identity.
-            const authRes = (await slackClient.auth.test()) as AuthTestResponse;
-            userId = authRes.user_id;
-            const userRes = (await slackClient.users.info({user: authRes.user_id})) as UsersInfoResponse;
-            botId = userRes.user!.profile!.bot_id!;
+            userId = auth.user_id;
+            botId = user.user!.profile!.bot_id!;
             teamRes = team;
-            const potentialTeam = await this.datastore.getTeam(team.id);
-            if (potentialTeam) {
-                existingTeam = potentialTeam;
-            }
         } catch (ex) {
             log.warn(`Failed to authenticate`, ex);
             throw ex;
         }
-        const teamEntry: TeamEntry = existingTeam || {
+        const existingTeam = (await this.datastore.getTeam(teamRes.id));
+        const teamEntry: TeamEntry = {
             id: teamRes!.id,
-            scopes: "", // Unknown
-            domain: "",
-            name: "",
-            user_id: "",
-            bot_id: "",
+            scopes: existingTeam ? existingTeam.scopes : "", // Unknown
+            domain: teamRes!.domain,
+            name: teamRes!.name,
+            user_id: userId,
+            bot_id: botId,
             status: "ok",
-            bot_token: "",
+            bot_token: token,
         };
-        teamEntry.domain = teamRes!.domain;
-        teamEntry.name = teamRes!.name;
-        teamEntry.bot_id = botId;
-        teamEntry.user_id = userId;
-        teamEntry.status = "ok";
-        teamEntry.bot_token = token;
         await this.datastore.upsertTeam(teamEntry);
         return teamRes!.id;
-
     }
 
     public async getClientForUser(teamId: string, matrixUser: string): Promise<WebClient|null> {
@@ -161,6 +138,7 @@ export class SlackClientFactory {
         this.puppets.set(key, client);
         return client;
     }
+
 
     private async createTeamClient(token: string) {
         const opts = this.config.slack_client_opts;
@@ -184,8 +162,10 @@ export class SlackClientFactory {
         });
         try {
             const teamInfo = (await slackClient.team.info()) as TeamInfoResponse;
+            const auth = (await slackClient.auth.test()) as AuthTestResponse;
+            const user = (await slackClient.users.info({user: auth.user_id})) as UsersInfoResponse;
             log.debug("Created new team client for", teamInfo.team.name);
-            return { slackClient, team: teamInfo.team };
+            return { slackClient, team: teamInfo.team, auth, user };
         } catch (ex) {
             throw Error("Could not create team client: " + ex.data.error);
         }
