cbcfab6dbb4fb97d0b23cf25bfec9fe8cfc9c91c
Half-Shot <will@half-shot.uk>
2019-09-12 11:00:02 +0100

    Add licences to file that are missing them

--- src/datastore/postgres/PgDatastore.ts
+++ src/datastore/postgres/PgDatastore.ts
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 // tslint thinks we can combine these two statements, but so far I have been unable to.
 import * as pgInit from "pg-promise";
 // tslint:disable-next-line: no-duplicate-imports
