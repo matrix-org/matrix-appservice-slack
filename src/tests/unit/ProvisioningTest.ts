3c775ac9a94fe0bc8cd58c155f1c6894c61bb98d
Half-Shot <will@half-shot.uk>
2019-09-24 14:34:50 +0100

    Add tests for provisioning getauth request

---
+++ src/tests/unit/ProvisioningTest.ts
@@ -0,0 +1,112 @@
+/*
+Copyright 2019 The Matrix.org Foundation C.I.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// tslint:disable: no-unused-expression no-any
+
+import { commands } from "../../Provisioning";
+import { FakeMain } from "../utils/fakeMain";
+import { expect } from "chai";
+import { FakeExpressResponse } from "../utils/fakeExpress";
+
+// tslint:disable-next-line: max-line-length
+const OAuthUrlRegex = /^https:\/\/slack\.com\/oauth\/authorize\?client_id=fakeid&redirect_uri=redir_prefix([0-9a-z-]+)%2Fauthorize&scope=(.*)&state=([0-9a-z-]+)$/;
+
+describe("Provisioning", () => {
+    describe("commands.authurl", () => {
+        it ("should not handle command with missing body parameters", async () => {
+            const fakeMain = new FakeMain();
+            const req = {
+                body: { },
+            };
+            const res = new FakeExpressResponse();
+            await commands.authurl.run(fakeMain as any, req as any, res as any);
+            expect(res.Status).to.equal(400);
+            expect(res.Json).to.deep.equal({
+                error: "Required parameter user_id missing",
+            });
+        });
+        it ("should not handle command with disabled oauth2", async () => {
+            const fakeMain = new FakeMain({ oauth2: false });
+            const req = {
+                body: {
+                    user_id: "foobar",
+                },
+            };
+            const res = new FakeExpressResponse();
+            await commands.authurl.run(fakeMain as any, req as any, res as any);
+            expect(res.Status).to.equal(400);
+            expect(res.Json).to.deep.equal({
+                error: "OAuth2 not configured on this bridge",
+            });
+        });
+        it ("should handle command with missing puppeting body parameter", async () => {
+            const fakeMain = new FakeMain({ oauth2: true });
+            const req = {
+                body: {
+                    user_id: "foobar",
+                },
+            };
+            const res = new FakeExpressResponse();
+            await commands.authurl.run(fakeMain as any, req as any, res as any);
+            expect(res.Status).to.equal(200);
+            expect(res.Json).to.exist;
+            const match = OAuthUrlRegex.exec(res.Json.auth_uri);
+            expect(match).is.not.null;
+            expect(match![1]).to.equal(match![3]);
+            expect(match![2]).to.equal(
+                "team%3Aread%2Cusers%3Aread%2Cchannels%3Ahistory%2Cchannels%3Aread%2Cfiles%3Awrite%3Auser%2Cchat%3Awrite%3Abot%2Cusers%3Aread%2Cbot",
+            );
+        });
+        it ("should handle command with missing puppeting body parameter set to false", async () => {
+            const fakeMain = new FakeMain({ oauth2: true });
+            const req = {
+                body: {
+                    user_id: "foobar",
+                    puppeting: "false",
+                },
+            };
+            const res = new FakeExpressResponse();
+            await commands.authurl.run(fakeMain as any, req as any, res as any);
+            expect(res.Status).to.equal(200);
+            expect(res.Json).to.exist;
+            const match = OAuthUrlRegex.exec(res.Json.auth_uri);
+            expect(match).is.not.null;
+            expect(match![1]).to.equal(match![3]);
+            expect(match![2]).to.equal(
+                "team%3Aread%2Cusers%3Aread%2Cchannels%3Ahistory%2Cchannels%3Aread%2Cfiles%3Awrite%3Auser%2Cchat%3Awrite%3Abot%2Cusers%3Aread%2Cbot",
+            );
+        });
+        it ("should handle command with missing puppeting body parameter set to true", async () => {
+            const fakeMain = new FakeMain({ oauth2: true });
+            const req = {
+                body: {
+                    user_id: "foobar",
+                    puppeting: "true",
+                },
+            };
+            const res = new FakeExpressResponse();
+            await commands.authurl.run(fakeMain as any, req as any, res as any);
+            expect(res.Status).to.equal(200);
+            expect(res.Json).to.exist;
+            const match = OAuthUrlRegex.exec(res.Json.auth_uri);
+            expect(match).is.not.null;
+            expect(match![1]).to.equal(match![3]);
+            expect(match![2]).to.equal(
+                "client",
+            );
+        });
+    });
+});
