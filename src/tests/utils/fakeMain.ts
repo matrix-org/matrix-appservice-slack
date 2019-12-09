f3b22e2ef74f5b22852b5c8259d674209a4b678a
Half-Shot <will@half-shot.uk>
2019-12-09 16:26:27 +0000

    Fix tests

--- src/tests/utils/fakeMain.ts
+++ src/tests/utils/fakeMain.ts
@@ -59,10 +59,10 @@ export class FakeMain {
 
     public async getExistingSlackGhost(userId: string) {
         if (userId === "@stranger:localhost") {
-            return new SlackGhost(this as unknown as Main, "12345", undefined, "@stranger:localhost", undefined);
+            return new SlackGhost(this.datastore, "12345", undefined, "@stranger:localhost", undefined);
         }
         if (userId === "@thing:localhost") {
-            return new SlackGhost(this as unknown as Main, "54321", undefined, "@thing:localhost", undefined);
+            return new SlackGhost(this.datastore, "54321", undefined, "@thing:localhost", undefined);
         }
         return null;
     }
