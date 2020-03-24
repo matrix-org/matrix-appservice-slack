7ddb5f04f1c8566fc4daf75e3c3b292950f6ba44
Christian Paul <info@jaller.de>
2020-03-24 21:34:40 +0100

    Add unit tests for MatrixUser.getDisplayName() and .getAvatarUrlForRoom()

--- src/tests/unit/MatrixUserTest.ts
+++ src/tests/unit/MatrixUserTest.ts
@@ -20,20 +20,87 @@ import { MatrixUser } from "../../MatrixUser";
 import { Main } from "../../Main";
 import { expect } from "chai";
 
+/**
+ * Given an array of users, this function creates an array of join events.
+ */
+const getStoredEventGenerator = (users: {
+    id?: string,
+    displayName?: string,
+    avatarUrl?: string,
+}[]) => {
+    const events = users.map((user) => ({
+        type: "m.room.member",
+        user_id: user.id,
+        content: {
+            avatar_url: user.avatarUrl,
+            displayname: user.displayName,
+            membership: "join",
+        }
+    }));
+    return (roomId: string, eventType: string, stateKey?: string) => {
+        // We were asked for one specifc user id
+        if (stateKey) {
+            return events.find((user) => user.user_id === stateKey);
+        }
+        // Return an array of all join events
+        return events;
+    }
+};
+
 describe("MatrixUser", () => {
     it("can construct", () => {
-        const user = new MatrixUser({} as Main, { user_id: "hello"});
-        expect(user.userId).to.equal("hello");
+        const user = new MatrixUser({} as Main, { user_id: "@alice:localhost"});
+        expect(user.userId).to.equal("@alice:localhost");
         expect(user.aTime).to.be.null;
     });
 
     describe("bumpATime", () => {
         it("will bump to the correct value", () => {
-            const user = new MatrixUser({} as Main, { user_id: "hello"});
+            const user = new MatrixUser({} as Main, { user_id: "@alice:localhost"});
             const now = Date.now() / 1000;
             user.bumpATime();
             // Can either be now, or now + 1
             expect(user.aTime).to.be.greaterThan(now - 1).and.lessThan(now + 2);
+        });
+    });
+
+    describe("getDisplayName", () => {
+        it("returns the user_id when no displayName is given", () => {
+            const getStoredEvent = getStoredEventGenerator([]);
+            const user = new MatrixUser({ getStoredEvent } as Main, { user_id: "@alice:localhost" });
+            expect(user.getDisplaynameForRoom("")).to.equal("@alice:localhost");
+        });
+        it("returns the displayName if one is given", () => {
+            const getStoredEvent = getStoredEventGenerator([
+                { id: "@alice:localhost", displayName: "Alice" },
+                { displayName: "Hatmaker" },
+            ]);
+            const user = new MatrixUser({ getStoredEvent } as Main, { user_id: "@alice:localhost" });
+            expect(user.getDisplaynameForRoom("")).to.equal("Alice");
+        });
+        it("returns 'displayName (userId)' if the display name isn't unique", () => {
+            const getStoredEvent = getStoredEventGenerator([
+                { displayName: "Alice" },
+                { id: "@alice:localhost", displayName: "Alice" },
+            ]);
+            const user = new MatrixUser({ getStoredEvent } as Main, { user_id: "@alice:localhost" });
+            expect(user.getDisplaynameForRoom("")).to.equal("Alice (@alice:localhost)");
+        });
+    });
+
+    describe("getAvatarUrlForRoom", () => {
+        it("returns undefined when no avatar is given", () => {
+            const getStoredEvent = getStoredEventGenerator([]);
+            const user = new MatrixUser({ getStoredEvent } as Main, { user_id: "@alice:localhost" });
+            expect(user.getAvatarUrlForRoom("")).to.be.null;
+        });
+        it("returns the avatar_url if one is given", () => {
+            const getStoredEvent = getStoredEventGenerator([
+                { id: "@alice:localhost", avatarUrl: "https://localhost/alice.png" },
+                { id: "@hatmaker:localhost" },
+            ]);
+            const user = new MatrixUser({ getStoredEvent } as Main, { user_id: "@alice:localhost" });
+            expect(user.getAvatarUrlForRoom("")).to.equal("https://localhost/alice.png");
         });
     });
 });
