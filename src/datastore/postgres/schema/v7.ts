b1961bb8ebb598d6d0a805e44973157c5a858220
Will Hunt <will@half-shot.uk>
2020-05-27 18:07:03 +0100

    Fix puppets being constrained by team / matrix id

---
+++ src/datastore/postgres/schema/v7.ts
@@ -0,0 +1,10 @@
+import { IDatabase } from "pg-promise";
+import { MatrixUser } from "matrix-appservice-bridge";
+
+// tslint:disable-next-line: no-any
+export async function runSchema(db: IDatabase<any>) {
+    // Drop constraints
+    await db.none(`
+        ALTER TABLE puppets DROP CONSTRAINT puppets_slackteam_key;
+        ALTER TABLE puppets DROP CONSTRAINT puppets_matrixuser_key;`);
+}
