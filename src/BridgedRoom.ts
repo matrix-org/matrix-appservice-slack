6b89d5a4d0b810b6e0afb248646298684e34baa8
Half-Shot <will@half-shot.uk>
2020-07-24 16:33:55 +0100

    Linting

--- src/BridgedRoom.ts
+++ src/BridgedRoom.ts
@@ -656,7 +656,7 @@ export class BridgedRoom {
         sneakyThis[key] = value;
         this.dirty = true;
     }
-    
+
     private async handleSlackMessageFile(file: ISlackFile, slackEventId: string, ghost: SlackGhost) {
         const maxUploadSize = this.main.config.homeserver.max_upload_size;
         const channelId = this.slackChannelId;
@@ -866,81 +866,11 @@ export class BridgedRoom {
             };
             return ghost.sendMessage(this.MatrixRoomId, matrixContent, channelId, eventTS);
         } else if (message.files) { // A message without a subtype can contain files.
-            const maxUploadSize = this.main.config.homeserver.max_upload_size;
             for (const file of message.files) {
-                if (!file.url_private) {
-                    // Cannot do anything with this.
-                    continue;
-                }
-
-                if (file.mode === "snippet") {
-                    let htmlString: string;
-                    try {
-                        htmlString = await rp({
-                            headers: {
-                                Authorization: `Bearer ${this.SlackClient!.token}`,
-                            },
-                            uri: file.url_private!,
-                        });
-                    } catch (ex) {
-                        log.error("Failed to download snippet", ex);
-                        continue;
-                    }
-                    let htmlCode = "";
-                    // Because escaping 6 backticks is not good for readability.
-                    // tslint:disable-next-line: prefer-template
-                    const code = "```" + `\n${htmlString}\n` + "```";
-                    if (file.filetype) {
-                        htmlCode = `<pre><code class="language-${file.filetype}'">`;
-                    } else {
-                        htmlCode = "<pre><code>";
-                    }
-                    htmlCode += substitutions.htmlEscape(htmlString);
-                    htmlCode += "</code></pre>";
-
-                    const messageContent = {
-                        body: code,
-                        format: "org.matrix.custom.html",
-                        formatted_body: htmlCode,
-                        msgtype: "m.text",
-                    };
-                    await ghost.sendMessage(this.matrixRoomId, messageContent, channelId, eventTS);
-                } else {
-                    if (maxUploadSize && file.size > maxUploadSize) {
-                        const link = file.public_url_shared ? file.permalink_public : file.url_private;
-                        log.info("File too large, sending as a link");
-                        const messageContent = {
-                            body: `${link} (${file.name})`,
-                            format: "org.matrix.custom.html",
-                            formatted_body: `<a href="${link}">${file.name}</a>`,
-                            msgtype: "m.text",
-                        };
-                        await ghost.sendMessage(this.matrixRoomId, messageContent, channelId, eventTS);
-                        continue;
-                    }
-                    // We also need to upload the thumbnail
-                    let thumbnailPromise: Promise<string> = Promise.resolve("");
-                    // Slack ain't a believer in consistency.
-                    const thumbUri = file.thumb_video || file.thumb_360;
-                    if (thumbUri && file.filetype) {
-                        thumbnailPromise = ghost.uploadContentFromURI(
-                            {
-                                mimetype: file.mimetype,
-                                title: `${file.name}_thumb.${file.filetype}`,
-                            },
-                            thumbUri,
-                            this.SlackClient!.token!,
-                        );
-                    }
-                    const fileContentUri = await ghost.uploadContentFromURI(
-                        file, file.url_private, this.SlackClient!.token!);
-                    const thumbnailContentUri = await thumbnailPromise;
-                    await ghost.sendMessage(
-                        this.matrixRoomId,
-                        slackFileToMatrixMessage(file, fileContentUri, thumbnailContentUri),
-                        channelId,
-                        eventTS,
-                    );
+                try {
+                    await this.handleSlackMessageFile(file, eventTS, ghost);
+                } catch (ex) {
+                    log.warn(`Couldn't handle Slack file, ignoring:`, ex);
                 }
             }
             // TODO: Currently Matrix lacks a way to upload a "captioned image",
