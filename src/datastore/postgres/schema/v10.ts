70d194847d2dd8ff4ba67df1731fb68041fd98d6
Will Hunt <will@half-shot.uk>
2020-09-17 15:47:53 +0100

    Add storage for sessions

---
+++ src/datastore/postgres/schema/v10.ts
@@ -0,0 +1,14 @@
+import { IDatabase } from "pg-promise";
+
+// tslint:disable-next-line: no-any
+export async function runSchema(db: IDatabase<any>) {
+
+    await db.none(`
+        CREATE TABLE encryption_sessions (
+            user_id TEXT NOT NULL,
+            device_id TEXT NOT NULL,
+            access_token TEXT NOT NULL,
+        );
+        CREATE UNIQUE INDEX encryption_sessions_idx ON reactions (user_id);
+    `);
+}
