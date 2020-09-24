db5876693a3659921c76ce92142bf70874c8a65e
Will Hunt <will@half-shot.uk>
2020-09-24 09:54:11 +0100

    Fix makeRedirectURL to handle missing slashes

--- src/OAuth2.ts
+++ src/OAuth2.ts
@@ -134,6 +134,6 @@ export class OAuth2 {
     }
 
     private makeRedirectURL(token: string): string {
-        return `${this.redirectPrefix}${token}/authorize`;
+        return path.join(this.redirectPrefix, token, "authorize");
     }
 }
