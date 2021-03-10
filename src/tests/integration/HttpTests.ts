ed3bf5fd2d56027d6a4569e00274a579ffa77a29
Will Hunt <will@half-shot.uk>
2021-03-10 11:04:02 +0000

    Update src/tests/integration/HttpTests.ts
    Co-authored-by: Christian Paul <christianp@matrix.org>

--- src/tests/integration/HttpTests.ts
+++ src/tests/integration/HttpTests.ts
@@ -27,7 +27,7 @@ describe("HttpTests", () => {
         harness = constructHarness();
     });
 
-    it("will respond 201 to a health check", async () => {
+    it("will respond 200 to a health check", async () => {
         await harness.main.run(57000);
         const res = await axios.get("http://localhost:57000/health");
         expect(res.status).to.equal(200);
