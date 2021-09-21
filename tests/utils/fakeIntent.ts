7826ba9b1c6cf057670917246e46526e07bfa604
Will Hunt <willh@matrix.org>
2021-09-21 12:14:18 +0100

    Number of small tweaks to tests

--- tests/utils/fakeIntent.ts
+++ tests/utils/fakeIntent.ts
@@ -1,16 +1,10 @@
 export class FakeIntent {
-    public getClient(): { getRoomIdForAlias: (alias: string) => { room_id: string } } {
-        return {
-            getRoomIdForAlias: this.cliGetRoomIdForAlias.bind(this),
-        };
-    }
-
-    private cliGetRoomIdForAlias(alias: string) {
+    public resolveRoom(alias: string) {
         if (alias === "#working:localhost") {
-            return { room_id: "!working:localhost" };
+            return "!working:localhost";
         }
         if (alias === "#working2:localhost") {
-            return { room_id: "!alsoworking:localhost" };
+            return "!alsoworking:localhost";
         }
         throw Error("Test says no room found");
     }
