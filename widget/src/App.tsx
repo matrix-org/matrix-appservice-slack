2f1fc9c0d7f4e3163d7ef83423a4208f4f49bace
Justin Carlson <justinc@element.io>
2023-02-08 14:08:59 -0500

    Add setup widget interface

--- widget/src/App.tsx
+++ widget/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+
 import { ProvisioningApp } from './ProvisioningApp';
+import { SlackApp } from './SlackApp';
 
 const App = () => {
     return <ProvisioningApp
         apiPrefix='/_matrix/provision'
         tokenName='slack-sessionToken'
     >
-        Ready!
+        <SlackApp/>
     </ProvisioningApp>;
 };
 
